01e9b281416dbca89dbe5a39dcaf0eba49c4901d
Denise Middleton <demiddleton@bellsouth.net>
2019-10-20 18:36:30 -0400

    psuedocode orm.js

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//Create a method to selectAll
+
+//Create a method to insertOne
+
+//Create a method to updateOne
+
+
+module.exports = orm;
