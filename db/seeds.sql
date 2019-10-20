4062a9927f45684bf9cc4a5f2eee36096e9a77e3
Denise Middleton <demiddleton@bellsouth.net>
2019-10-20 18:13:32 -0400

    creadted seeds for the db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Plain Hamburger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Pimento Cheese Hamburger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Jalapeno Hamburger', false);
