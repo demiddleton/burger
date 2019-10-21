dcb95712bb13a8802fcf444f97b2eb0afa22e86e
Denise Middleton <demiddleton@bellsouth.net>
2019-10-21 17:52:05 -0400

    created code for createOne

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,37 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+       arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }    
+
 var orm = {
 
    //Create a method to selectAll
@@ -33,17 +64,25 @@ var orm = {
           cb(result);
         });
       },
+
+      //Create a method to updateOne
+      update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
     
-
-
-
-
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }    
 };
-
-
-
-
-//Create a method to updateOne
-
 
 module.exports = orm;
