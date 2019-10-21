dba9477cd98eac81b6c4d14297d142bb24b2490f
Denise Middleton <demiddleton@bellsouth.net>
2019-10-21 17:42:52 -0400

    created code for selectAll and insertOne

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,47 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-//Create a method to selectAll
+var orm = {
 
-//Create a method to insertOne
+   //Create a method to selectAll
+    selectAll: function (tableName) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableName], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+
+    //Create a method to insertOne
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+    
+
+
+
+
+};
+
+
+
 
 //Create a method to updateOne
 
