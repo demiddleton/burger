d22b44ab330e8bccda549775a6ce2714929833c6
Denise Middleton <demiddleton@bellsouth.net>
2019-10-22 19:31:16 -0400

    created code to call ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,24 @@
 //Grab the orm from the config folder
 var orm = require("../config/orm.js");
 
-module.exports = function(app) {
+var burgers = {
+	selectAll: function(cb){
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		});
+	},
 
-    
+	insertOne: function(cols,vals,cb){
+		orm.insertOne("burgers", cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	update: function(objColVals, condition, cb){
+		orm.update("burgers", objColVals, condition, function(res){
+			cb(res);
+		});
+	},
 };
+
+module.exports = burgers;
--- server.js
+++ server.js
@@ -25,7 +25,9 @@ var routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
