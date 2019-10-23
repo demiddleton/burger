3af42d397b95acc8be7557fb6757aa5d911ee2b4
Denise Middleton <demiddleton@bellsouth.net>
2019-10-23 17:34:11 -0400

    corrected the syntax in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,10 @@ USE burgers_db;
 
 CREATE TABLE burger
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+ 
+ 
