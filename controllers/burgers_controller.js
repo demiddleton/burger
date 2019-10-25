401603b18aca15f5396bf66674744990acf5cab2
Denise Middleton <demiddleton@bellsouth.net>
2019-10-24 21:41:09 -0400

    pushing updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,45 +5,28 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-router.get("/", function (req, res) {
-    burger.selectAll(function(data) {
-        var allBurgers = {
-            burger: data
-        };
-        console.log(allBurgers);
-        res.render("index", allBurgers);
-    });
+
+router.get("/burger", function(req,res){
+	burger.selectAll(function(data){
+		var hbsObject = {
+			burger: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
 });
-  
-// Add new burger to the db.
-router.post("/api/burgers", function(req, res) {
+
+router.post("/api/burger", function(req, res) {
     burger.create([
       "burger_name", "devoured"
     ], [
       req.body.burger_name, req.body.devoured
     ], function(result) {
-      // Send back the ID of the new quote
+      // Return ID of the Burger
       res.json({ id: result.insertId });
     });
   });
-
-  //Update devoured state
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
   
-    console.log("condition", condition);
-  
-    burger.update({
-      devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
 
 
 module.exports = router;
