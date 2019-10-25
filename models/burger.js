dae4b0f19ff030c28146370f0b3ea3cf7ecfad9e
Denise Middleton <demiddleton@bellsouth.net>
2019-10-25 18:41:13 -0400

    updated the burger.js file to include the deleteOne function

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,29 @@
 //Grab the orm from the config folder
 var orm = require("../config/orm.js");
-
 var burger = {
-	selectAll: function(cb){
-		orm.selectAll("burger", function(res){
-			cb(res);
-		});
-	},
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
 
-	insertOne: function(cols,vals,cb){
-		orm.insertOne("burger", cols, vals, function(res){
-			cb(res);
-		});
-	},
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
 
-	update: function(objColVals, condition, cb){
-		orm.update("burger", objColVals, condition, function(res){
-			cb(res);
-		});
-	},
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
 module.exports = burger;
