d4c66a872f85e96ea6b28a0452bc19d018cfd2eb
Denise Middleton <demiddleton@bellsouth.net>
2019-10-25 18:37:41 -0400

    updated the orm file to delete from the table

--- config/orm.js
+++ config/orm.js
@@ -35,55 +35,65 @@ function printQuestionMarks(num) {
 var orm = {
 
    //Create a method to selectAll
-    selectAll: function (tableName, cb) {
-        var queryString = "SELECT * FROM ??; ";
-        connection.query(queryString, [tableName], function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            cb(result);
-        });
-    },
+   selectAll: function(table, cb) {
+    var dbQuery = "SELECT * FROM " + table + ";";
+
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
 
     //Create a method to insertOne
     insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-    
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-    
-        console.log(queryString);
-    
-        connection.query(queryString, vals, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
-      },
-
-      //Create a method to updateOne
-      update: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-    
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-    
-        console.log(queryString);
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
-      }    
-};
-
-module.exports = orm;
+      var dbQuery =
+        "INSERT INTO " +
+        table +
+        " (" +
+        cols.toString() +
+        ") " +
+        "VALUES (" +
+        createQmarks(vals.length) +
+        ") ";
+  
+      console.log(dbQuery);
+      connection.query(dbQuery, vals, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        cb(res);
+      });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+      var dbQuery =
+        "UPDATE " +
+        table +
+        " SET " +
+        translateSql(objColVals) +
+        " WHERE " +
+        condition;
+  
+      console.log(dbQuery);
+  
+      connection.query(dbQuery, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        cb(res);
+      });
+    },
+    deleteOne: function(table, condition, cb) {
+      var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+      console.log(dbQuery);
+  
+      connection.query(dbQuery, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        cb(res);
+      });
+    }
+  };
+  module.exports = orm;
